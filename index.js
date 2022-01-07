044757665c444aa3125bfae98edcac96cf86f3b4
이국진 <cptkuk91@gmail.com>
2022-01-08 05:07:35 +0900

    55분 떠상 봇

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
         btcresult.data.bpi.USD.rate
 
         const result = await axios.post("https://discord.com/api/webhooks/929083189895516191/bU-waoMb_9lbRG10nI3QmA5IGnD-ACc-AdtcXwn7ozEBMLRI2_KeZxOSQXoGlRWSQgwN", {
-            "content": "----------------------------------------" + btcresult.data.bpi.USD.rate
+            "content": "----------------------------------------"
         });
         console.info("디스코드 작동 중");
     }
