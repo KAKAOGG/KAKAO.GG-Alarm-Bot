8bebf6d3b63c275333a70eb59008f3a870344d9e
이세진 <cptkuk91>
2022-02-07 06:23:37 +0900

    1차 테스트 완료

--- index.js
+++ index.js
@@ -1,92 +1,90 @@
 const axios = require("axios");
 
+require('dotenv').config();
+
 exports.handler = async (event) => {
-
-    // 카제로스 서버 시작
-    try{
-        const btcresult = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-        btcresult.data.bpi.USD.rate
-
-        const result = await axios.post("https://discord.com/api/webhooks/929104254822658118/fZ2GVMQE6ZQDGdSpckhFMLQGyc3K4KUbcilGT9qSC1Uo0b3WuYXZ9vhoVGVLqA5-dPVO", {
-            "content": "---------------카제로스 떠돌이 상인 마감--------------------"
-        });
-        console.info("디스코드 작동 중");
-    }
-    catch(err){
-        console.log("디스코드 작동 중 에러", err);
-    };
-
-    // 루페온 서버 시작
-    try{
-        const btcresult = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-        btcresult.data.bpi.USD.rate
-
-        const result = await axios.post("https://discord.com/api/webhooks/929104684235509861/AB_1y9coA_BleT7W__sBJVYepVFrH67eqL3cBwan0LIdMNUa6GwKsId1qcfcMYC7dn2g", {
-            "content": "---------------루페온 떠돌이 상인 마감--------------------"
-        });
-        console.info("디스코드 작동 중");
-    }
-    catch(err){
-        console.log("디스코드 작동 중 에러", err);
-    };
-
-    // 카단 서버 시작
-    try{
-        const btcresult = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-        btcresult.data.bpi.USD.rate
-
-        const result = await axios.post("https://discord.com/api/webhooks/929104816003776602/eSWbxySmr0dDa3UR_u2VxOlhJSUw1w-d15qQT9FUq8l6o1KM1bOcXhUl6AH_QYxrcQW3", {
-            "content": "---------------카단 떠돌이 상인 마감--------------------"
-        });
-        console.info("디스코드 작동 중");
-    }
-    catch(err){
-        console.log("디스코드 작동 중 에러", err);
-    };
-
-    // 아브렐슈드 서버 시작
-    try{
-        const btcresult = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-        btcresult.data.bpi.USD.rate
-
-        const result = await axios.post("https://discord.com/api/webhooks/929104926557229087/b9pJUkRkhYu3IYrSixfvIXVwIybbJQobBln0yzVVgHdyDPYUXlw0kkNvUnWRC2TqtzyN", {
-            "content": "---------------아브렐슈드 떠돌이 상인 마감--------------------"
-        });
-        console.info("디스코드 작동 중");
-    }
-    catch(err){
-        console.log("디스코드 작동 중 에러", err);
-    };
-
-    // 아만 서버 시작
-    try{
-        const btcresult = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-        btcresult.data.bpi.USD.rate
-
-        const result = await axios.post("https://discord.com/api/webhooks/929105044157112330/yPpmAhh_k62o52zUQz2rSvVXORq9ZIatUYLoF6ko5so1guUGYlccCHZe-AMr5yWKTGHK", {
-            "content": "---------------아만 떠돌이 상인 마감--------------------"
-        });
-        console.info("디스코드 작동 중");
-    }
-    catch(err){
-        console.log("디스코드 작동 중 에러", err);
-    };
-
-    // 실리안 서버 시작
-    try{
-        const btcresult = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-        btcresult.data.bpi.USD.rate
-
-        const result = await axios.post("https://discord.com/api/webhooks/929105152286265344/HkHovmi5g4VZPknBuUq59OqYgFM-VhinRcYjaVOuYMyfYscht1ri5lnYdpqqpOqiJwoi", {
-            "content": "---------------실리안 떠돌이 상인 마감--------------------"
-        });
-        console.info("디스코드 작동 중");
-    }
-    catch(err){
-        console.log("디스코드 작동 중 에러", err);
-    };
-
     // TODO implement
+    try{
+        const groupOne = {
+            "content": "아르테미스, 욘, 베른 북부, 베른 남부 4개 지역입니다."
+        }
+
+        const groupTwo = {
+            "content": "페이튼, 루테란 동부, 유디아, 애니츠, 슈샤이어 5개 지역입니다."
+        }
+
+        const groupThree = {
+            "content": "루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카 6개 지역입니다."
+        }
+
+        const groupOneTwo = {
+            "content": "아르테미스, 욘, 베른 북부, 베른 남부, 페이튼, 루테란 동부, 유디아, 애니츠, 슈샤이어 9개 지역입니다."
+        }
+
+        const groupOneThree = {
+            "content": "아르테미스, 욘, 베른 북부, 베른 남부, 루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카 10개 지역입니다."
+        }
+
+        const groupOneFour = {
+            "content": "아르테미스, 욘, 베른 북부, 베른 남부, 로웬 5개 지역입니다."
+        }
+
+        const groupTwoThree = {
+            "content": "페이튼, 루테란 동부, 유디아, 애니츠, 슈샤이어, 루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카 11개 지역입니다."
+        }
+
+        const groupTwoFour = {
+            "content": "페이튼, 루테란 동부, 유디아, 애니츠, 슈샤이어, 로웬 6개 지역입니다."
+        }
+
+        const groupThreeFour = {
+            "content": "루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카, 로웬 7개 지역입니다."
+        }
+
+        const discordUrl = process.env.DISCORD_BOT_TEST_URI;
+        
+        if(event.bot === "groupOne"){
+            const result = await axios.post(discordUrl, groupOne);
+        }
+
+        if(event.bot === "groupTwo"){
+            const result = await axios.post(discordUrl, groupTwo);
+        }
+
+        if(event.bot === "groupThree"){
+            const result = await axios.post(discordUrl, groupThree);
+        }
+
+        if(event.bot === "groupOneTwo"){
+            const result = await axios.post(discordUrl, groupOneTwo);
+        }
+
+        if(event.bot === "groupOneThree"){
+            const result = await axios.post(discordUrl, groupOneThree);
+        }
+
+        if(event.bot === "groupOneFour"){
+            const result = await axios.post(discordUrl, groupOneFour);
+        }
+
+        if(event.bot === "groupTwoThree"){
+            const result = await axios.post(discordUrl, groupTwoThree);
+        }
+
+        if(event.bot === "groupTwoFour"){
+            const result = await axios.post(discordUrl, groupTwoFour);
+        }
+
+        if(event.bot === "groupThreeFour"){
+            const result = await axios.post(discordUrl, groupThreeFour);
+        }
+
+        console.log("정상 작동");
+    }
+    catch(err){
+        console.err("디스코드 봇 에러 발생");
+    };
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
