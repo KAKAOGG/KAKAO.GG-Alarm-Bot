f51203ab2d19e3226a6790524888597c9fcaf63f
cptkuk91 <cptkuk91@gmail.com>
2022-05-08 14:44:50 +0900

    엘가시아 수정

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ exports.handler = async (event) => {
 
     const groupOneTwo = {
       content:
-        "```아르테미스, 욘, 베른 북부, 베른 남부, 페이튼, 루테란 동부, 유디아, 애니츠, 슈샤이어 9개 지역입니다```",
+        "```아르테미스, 욘, 베른 북부, 베른 남부, 엘가시아, 페이튼, 루테란 동부, 유디아, 애니츠, 슈샤이어 9개 지역입니다```",
     };
 
     const groupOneThree = {
       content:
-        "```아르테미스, 욘, 베른 북부, 베른 남부, 루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카 10개 지역입니다```",
+        "```아르테미스, 욘, 베른 북부, 베른 남부, 엘가시아, 루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카 10개 지역입니다```",
     };
 
     const groupOneFour = {
@@ -50,7 +50,7 @@ exports.handler = async (event) => {
         "```루테란 서부, 루테란 동부, 토토이크, 아르데타인, 로헨델, 파푸니카, 로웬 7개 지역입니다```",
     };
 
-    const discordUrl = "DISCORD UPDATE Abre Server";
+    const discordUrl = "WEB_HOOK";
 
     if (event.bot === "groupOne") {
       const result = await axios.post(discordUrl, groupOne);
